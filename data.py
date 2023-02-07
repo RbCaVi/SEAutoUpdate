471190af7e6f1dc9374fc9afbe24b10e9ef48690
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2023-02-06 19:36:20 -0500

    Update data.py
    A bit of refactoring and comments

--- data.py
+++ data.py
@@ -1,31 +1,39 @@
-import json,shutil,os,subprocess,signal
+import json
+import os
+import sys
+import re
 
 import util
 import fix
 
-import sys
 
 regen=False
 
 def getmods():
-  mods=[*filter(lambda x:x.is_dir(),os.scandir(os.path.join(util.fdir,'mods')))]
-#  mods=[mod.name.rsplit('_',maxsplit=1)[0] for mod in mods]
-  nmods=[]
+  # get the list of mods in factorio
+  mods=[
+    *filter(lambda x:x.is_dir() or x.name.endswith('.zip'),
+      os.scandir(os.path.join(util.fdir,'mods'))
+    )
+  ]
+  nmods=[] # the list of mod names (no extension or version)
   for mod in mods:
     if mod.name.endswith('.zip'):
       nmods.append(mod.name.rsplit('_',maxsplit=1)[0])
     else:
       with open(os.path.join(mod.path,'info.json')) as f:
-        nmods.append(json.load(f)['name'])
+        nmods.append(json.load(f)['name']) # get the mod's name from its info.json
   return nmods
 
 if regen:
+  import shutil
   try:
-    shutil.rmtree(os.path.join(util.fdir,'mods/datalogger'))
+    # clear any old datalogger mod
+    shutil.rmtree(os.path.join(util.fdir,'mods','datalogger'))
   except:
     pass
 
-  info={
+  info={ # the data in the datalogger mod's info.json
     "name": "datalogger",
     "version": "0.0.1",
     "title": "Log the entire data.raw table",
@@ -33,49 +41,53 @@ if regen:
     "factorio_version": "1.1",
     "dependencies": getmods()
   }
+  # add the mod to factorio
   shutil.copytree('datalogger',os.path.join(util.fdir,'mods/datalogger'))
   with open(os.path.join(util.fdir,'mods/datalogger/info.json'),'w') as f:
     json.dump(info,f,indent=2)
 
-d=os.getcwd()
+d=os.getcwd() # save the cwd to restore it later
 os.chdir(util.fdir)
-if util.qpy:
+
+# outf is the (possibly cached) output from factorio
+if util.qpy or not regen:
   outf=open(os.path.join(util.fdir,'factorio-current.log'))
 else:
-  if regen:
-    subprocess.run(util.fexe+['--create','saves/protodump.zip'])
-    fproc=subprocess.Popen(util.fexe+['--start-server','saves/protodump.zip'],stdout=subprocess.PIPE,text=True)
-    outf=fproc.stdout
-  else:
-    #fproc=subprocess.Popen(['cat','factorio-current.log'],stdout=subprocess.PIPE,text=True)
-    outf=open(os.path.join(util.fdir,'factorio-current.log'))
-  #outf=fproc.stdout
+  import subprocess
+  subprocess.run(util.fexe+['--create','saves/protodump.zip']) # create a dummy save
+  fproc=subprocess.Popen(util.fexe+['--start-server','saves/protodump.zip'],stdout=subprocess.PIPE,text=True)
+  outf=fproc.stdout
 
 fout=''
 while True:
   out=outf.readline()
-#  print(out,end='')
   fout=fout+out
-  if 'Hosting game at' in fout:
+  if 'Hosting game at' in fout[-30:]+out:
+    del fout
     break
 
 outf.close()
 
 if not util.qpy and regen:
+  import signal
   fproc.send_signal(signal.SIGINT)
 
+# get just the data i need
 data=fout.split('__datalogger__/data-final-fixes.lua')[2]
 data=data.split(' ',maxsplit=1)[1].rsplit('\n',maxsplit=1)[0]
+
 data=json.loads(data)
+
+# fix the recipes and tech
 data['recipe']={x:fix.fixrecipe(data['recipe'][x]) for x in data['recipe']}
 data['technology']={x:fix.fixtech(data['technology'][x]) for x in data['technology']}
-#print(fout)
 
 os.chdir(d)
 
 import process
 
 def init():
+    # creates pdata
     global pdata
     pdata={}
     pdata['recipe']={x:process.processrecipe(x) for x in data['recipe']}
